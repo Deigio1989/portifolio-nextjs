89ebc226c9bef94715c438e2ba3e3ccaee05189e
Diego Sanchez <diegosmtattoo@gmail.com>
2025-03-17 00:22:49 -0300

    fix: Dark theme padrão

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,18 +1,18 @@
 // theme.ts
-export const lightTheme = {
-  background: "#ffffff",
-  foreground: "#161818",
-  color1: "#161818", // Principal
-  color2: "#6b6b6b", // Secundário (cinza escuro)
-  green: "#27ae60",
-  lightGreen: "#2ecc71",
-};
-
 export const darkTheme = {
   background: "#161818",
   foreground: "#ededed",
   color1: "#ededed", // Principal
   color2: "#aaaaaa", // Secundário (cinza claro)
+  green: "#27ae60",
+  lightGreen: "#2ecc71",
+};
+
+export const lightTheme = {
+  background: "#ffffff",
+  foreground: "#161818",
+  color1: "#161818", // Principal
+  color2: "#6b6b6b", // Secundário (cinza escuro)
   green: "#27ae60",
   lightGreen: "#2ecc71",
 };
